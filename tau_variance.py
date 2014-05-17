e873e5ce5317a44e28191b66179e01d238fbb7b7
jeremyvoltz <jeremyvoltz@gmail.com>
2014-05-17 12:13:18 -0400

    Added new modelling test
    This file fixes space variables, and then tests how long it takes to
    get to it’s ending edge with different time variables, and gives the
    variance, as well as graphing these beginnings.
    
    In its current form, the space variables are guaranteed to have
    alternating sign (to test a hypothesis I had, that it would hit them
    all correctly, which it doesn’t!)

---
+++ tau_variance.py
@@ -0,0 +1,143 @@
+from polymer import *
+import math
+import random
+from datetime import datetime
+import sys
+import traceback
+import pickle
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from counter import Counter
+import numpy as np
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# create a file handler
+handler = logging.FileHandler('trials.log')
+handler.setLevel(logging.INFO)
+
+# create a logging format
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# add the handlers to the logger
+logger.addHandler(handler)
+
+
+
+def most_common(lst):
+    """return a tuple (most_common_element, count_of_element_in_list)
+    """
+    data = Counter(lst)
+    return data.most_common(1)[0]
+
+def perfect_path(pol, end, size):
+    path = [0]
+    if end > 0:
+        om = discretize(pol.spacevar[size:size+end])
+
+    while path[-1] < end:
+        sigma = next_run(om, path[-1]+1)
+        if sigma <= next_run(pol.timevar, len(path)-1):
+            # path += [path[-1]+next_run(om, path[-1]+next_run(om, path[-1]+1))]*
+            pass
+        pass
+
+def discretize(l):
+    ret = []
+    for e in l:
+        if e > 0:
+            ret.append(1)
+        else:
+            ret.append(-1)
+    return ret
+
+def next_run(l, i):
+    j = i+1
+    while l[j] == l[i]:
+        j+=1
+    return j-i
+
+def trial(pol, size):
+
+    pol.timevar = None
+    pol.make_environment()
+    pol.compute_actions()
+
+    path = pol.compute_path()
+    tau = path.index(path[-1])
+
+    plt.plot(range(tau), path[:tau])
+
+    missed_sign = 0
+    bad_spots = []
+
+    for i in range(tau):
+        if pol.spacevar[path[i]+size]*pol.timevar[i] < 0:
+            missed_sign += 1
+            bad_spots.append(path[i])
+
+    # logger.info("omega: %s", pol.spacevar[:20])
+
+    return tau/float(path[-1]), missed_sign, bad_spots
+
+def runtrials(trials, size):
+    startTime = datetime.now()
+
+    logger.info("Trials initiated.")
+
+    polymer = Polymer(size, logger)
+
+    # Use just plus or minus 1 as space variables for clearer picture
+    polymer.spacevar = [(-1)**(i % 2) * random.uniform(0,1) for i in xrange(2 * size + 1)] 
+    taus = []
+    missed_signs = []
+    bad_spots = []
+
+    for _ in range(trials):
+        t, m, b = trial(polymer, size)
+        taus.append(t)
+        missed_signs.append(m)
+        bad_spots += b
+        bad_spots.sort()
+
+
+
+    plt.legend(range(1, trials+1), loc='upper left')
+    plt.savefig('./plots/taus:'+str(size)+'-'+str(datetime.now())+'.pdf')
+
+
+    avg =  sum(taus)/float(len(taus))
+    var = variance(taus, avg)
+
+    email_message = ("Program: Variance of taus up to ending edge" +'\n' 
+                     "Start: " + str(startTime) + '\n' 
+                     "Finish: " + str(datetime.now()) + '\n' 
+                     "Runtime: " + str(datetime.now() - startTime) + '\n' 
+                     "Size: " + str(size) + '\n' 
+                     "Number of Trials: " + str(trials) + '\n' 
+                     "Avg for taus: " + str(avg) +'\n' 
+                     "variance for taus: " + str(var) + '\n' 
+                     "missed_signs: " + str(missed_signs) + '\n'
+                     "Bad sites: " + str(bad_spots)
+                     )
+
+    logger.info("Test of string %s, %s", 3, 4)
+
+    logger.info(email_message)
+
+def variance(l, avg):
+    return sum([(e - avg) ** 2 for e in l])/float(len(l))
+
+if __name__ == '__main__':
+    try:
+        runtrials(eval(sys.argv[1]), eval(sys.argv[2]))
+    except:
+        logger.warning(traceback.format_exc())
+
+# cProfile.run(script)
